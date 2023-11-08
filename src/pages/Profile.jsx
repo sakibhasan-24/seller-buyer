9a4cd05383812b7f28315a82023368faa084a096
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-08 12:29:10 +0600

    add edit button functional

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -13,6 +13,7 @@ export default function Profile() {
     auth.signOut();
     navigate("/");
   };
+  const [isEdit, setIsEdit] = useState(false);
   return (
     <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
       <h1 className="text-3xl text-center font-bold mt-4">My Profile</h1>
@@ -30,12 +31,17 @@ export default function Profile() {
             name="email"
             id="email"
             value={formData.email}
-            className="w-full mb-5 px-4 py-2 text-2xl text-gray-900 bg-white border border border-gray-400 rounded-xl transition duration-500 ease-in-out"
+            className="w-full mb-5 px-4 py-2 text-2xl text-gray-900 bg-white  border border-gray-400 rounded-xl transition duration-500 ease-in-out"
           />
           <div className="flex justify-between mb-6 whitespace-nowrap text-sm lg:text-lg">
             <p className="flex items-center ">
               Need Motification?{" "}
-              <span className="text-red-600 hover:text-red-400">Edit</span>{" "}
+              <span
+                onClick={() => setIsEdit(!isEdit)}
+                className="text-red-600 hover:text-red-950 cursor-pointer font-bold"
+              >
+                {isEdit ? "save the changes" : "edit"}
+              </span>{" "}
             </p>
             <p
               className="bg-blue-500 px-2 py-1 text-white cursor-pointer"
