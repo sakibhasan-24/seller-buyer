26c9415f511cc6b8afc4b9bd8ffb6625689ca66a
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-21 02:52:38 +0600

    show fetched data from firebase db and create a card

--- src/pages/ExistingItem.jsx
+++ src/pages/ExistingItem.jsx
@@ -1,7 +1,47 @@
+import moment from "moment";
+import { MdLocationOn } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 export default function ExistingItem({ item, id }) {
+  //   console.log(item.createdTime);
+  const createTime = item.createTime?.toDate();
   return (
-    <div>
-      <h1>{item.name}</h1>
-    </div>
+    <li className="  flex flex-col my-8 justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden">
+      <Link className="contents" to={`/category/${item.type}/${id}`}>
+        <img
+          className="w-full h-[150px] object-cover hover:scale-105 transition-scale duration-200 ease-in"
+          src={item.imagesUrl[0]}
+          alt="userImage"
+          loading="lazy"
+        />
+        <span className="bg-blue-400 shadow-xl font-bold relative -top-36 left-0 text-xs uppercase text-black px-4 py-2 rounded-md">
+          {moment().startOf(`${createTime}`).fromNow()}
+        </span>
+
+        <div className="w-full p-[10px]">
+          <div className="flex items-center space-x-2">
+            <MdLocationOn className="h-6 w-6 text-blue-600" />
+            <p className="text-gray-600 font-bold truncate">{item.address}</p>
+          </div>
+          <p className="font-semibold  mt-2 text-xl m-0 text-violet-800">
+            {item.name}
+          </p>
+          <p className="font-semibold text-lg">
+            ${item.offer ? item.discountePrice : item.regularPrice}
+            {item.type === "rent" ? "/month" : ""}
+          </p>
+          <div>
+            <div className="flex items-center space-x-7">
+              <div className="flex items-center text-xs font-bold space-x-1">
+                <p>{item.bedrooms} beds</p>
+              </div>
+              <div className="flex items-center text-xs font-bold space-x-1">
+                <p>{item.bathRooms} baths</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -147,7 +147,7 @@ export default function Profile() {
             <h1 className="text-2xl text-center font-bold text-gray-700">
               my listing {foundData.length}
             </h1>
-            <ul>
+            <ul className="sm:grid  grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-12 my-6 ">
               {foundData.map((item) => (
                 <ExistingItem key={item.id} id={item.id} item={item.data} />
               ))}
